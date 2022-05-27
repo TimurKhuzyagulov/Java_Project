24a1696ee92f7548f94ba7d1159bd71f431c5c39
TimurKhuzyagulov <106373101+TimurKhuzyagulov@users.noreply.github.com>
2022-05-27 13:16:48 +0500

    Add files via upload

---
+++ src/main/java/Option.java
@@ -0,0 +1,29 @@
+public class Option {
+    long id;
+    String text;
+    boolean correct;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isCorrect() {
+        return correct;
+    }
+
+    public void setCorrect(boolean correct) {
+        this.correct = correct;
+    }
+}
---
+++ src/main/java/Quiz.java
@@ -0,0 +1,29 @@
+public class Quiz {
+    int id;
+    String name;
+    Question[] questionList;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Question[] getQuestionList() {
+        return questionList;
+    }
+
+    public void setQuestionList(Question[] questionList) {
+        this.questionList = questionList;
+    }
+}
